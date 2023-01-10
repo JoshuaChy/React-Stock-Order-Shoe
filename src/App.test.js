0d02e1e618ce58262c9a79667a82b9a176e439f1
JoshuaChy <69682057+JoshuaChy@users.noreply.github.com>
2023-01-10 10:16:10 +0700

    3 files added

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import assert from 'assert';
+import Data from './utils/data';
+import StockData from './utils/StockData';
+
+describe('Add Shoe Test', function () {
+  it('should be able to display stock available', async function () {
+    const StockInstance = StockData(Data);
+
+    assert.strictEqual(1, 1);
+  });
+});
